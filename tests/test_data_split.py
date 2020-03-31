5f6b8562aa942b8c5bc2f373d2960bc278204dd8
leungyukshing <jacky14.liang@gmail.com>
2020-04-01 00:05:59 +0800

    fix: add by_user test for data_split && fix some typo

--- tests/test_data_split.py
+++ tests/test_data_split.py
@@ -26,6 +26,17 @@ def generate_data():
     data = pd.DataFrame(data)
     return data
 
+def generate_data_by_user():
+    data = {
+        DEFAULT_USER_COL: [0, 0, 0, 0, 1, 1, 1, 1],
+        DEFAULT_ORDER_COL: [0, 1, 2, 2, 3, 4, 5, 5],
+        DEFAULT_TIMESTAMP_COL: [100, 200, 300, 300, 400, 500, 600, 600],
+        DEFAULT_ITEM_COL: [10, 20, 20, 30, 40, 50, 40, 60],
+        DEFAULT_RATING_COL: [1, 1, 1, 1, 1, 1, 1, 1],
+    }
+    data = pd.DataFrame(data)
+    return data
+
 class TestDataSplit(unittest.TestCase):
         
     def test_temporal_split(self):
@@ -67,6 +78,46 @@ class TestDataSplit(unittest.TestCase):
         self.assertEqual(tp_test.iloc[1,1], 5)
         self.assertEqual(tp_test.iloc[1,2], 500)
         self.assertEqual(tp_test.iloc[1,3], 10)
+        
+    def test_temporal_split_by_user(self):
+        testdata = generate_data_by_user()
+        
+        # check shape
+        self.assertEqual(testdata.shape[0], 8)
+        self.assertEqual(testdata.shape[1], 5)
+        
+        data = temporal_split(testdata, test_rate=0.1, by_user=True)
+        # check shape
+        self.assertEqual(data.shape[0], 8)
+        self.assertEqual(data.shape[1], 6)
+        
+        # check number
+        tp_train = data[data[DEFAULT_FLAG_COL] == "train"]
+        self.assertEqual(tp_train.shape[0], 4)
+        tp_validate = data[data[DEFAULT_FLAG_COL] == "validate"]
+        self.assertEqual(tp_validate.shape[0], 2)
+        tp_test = data[data[DEFAULT_FLAG_COL] == "test"]
+        self.assertEqual(tp_test.shape[0], 2)
+        
+        # check validate
+        self.assertEqual(tp_validate.iloc[0,0], 0)
+        self.assertEqual(tp_validate.iloc[0,1], 2)
+        self.assertEqual(tp_validate.iloc[0,2], 300)
+        self.assertEqual(tp_validate.iloc[0,3], 20)
+        self.assertEqual(tp_validate.iloc[1,0], 1)
+        self.assertEqual(tp_validate.iloc[1,1], 5)
+        self.assertEqual(tp_validate.iloc[1,2], 600)
+        self.assertEqual(tp_validate.iloc[1,3], 40)
+        
+        # check test
+        self.assertEqual(tp_test.iloc[0,0], 0)
+        self.assertEqual(tp_test.iloc[0,1], 2)
+        self.assertEqual(tp_test.iloc[0,2], 300)
+        self.assertEqual(tp_test.iloc[0,3], 30)
+        self.assertEqual(tp_test.iloc[1,0], 1)
+        self.assertEqual(tp_test.iloc[1,1], 5)
+        self.assertEqual(tp_test.iloc[1,2], 600)
+        self.assertEqual(tp_test.iloc[1,3], 60)
     
     def test_temporal_basket_split(self):
         testdata = generate_temporal_testdata()
@@ -99,6 +150,50 @@ class TestDataSplit(unittest.TestCase):
         self.assertEqual(tp_test.iloc[1, 1], 5)
         self.assertEqual(tp_test.iloc[1, 2], 500)
         self.assertEqual(tp_test.iloc[1, 3], 10)
+        
+    def test_temporal_basket_split_by_user(self):
+        testdata = generate_data_by_user()
+        
+        data = temporal_basket_split(testdata, test_rate=0.1, by_user=True)
+        # check shape
+        self.assertEqual(data.shape[0], 8)
+        self.assertEqual(data.shape[1], 6)
+        
+        # check number
+        tp_train = data[data[DEFAULT_FLAG_COL] == "train"]
+        self.assertEqual(tp_train.shape[0], 2)
+        tp_validate = data[data[DEFAULT_FLAG_COL] == "validate"]
+        self.assertEqual(tp_validate.shape[0], 2)
+        tp_test = data[data[DEFAULT_FLAG_COL] == "test"]
+        self.assertEqual(tp_test.shape[0], 4)
+        
+        # check validate
+        self.assertEqual(tp_validate.iloc[0, 0], 0)
+        self.assertEqual(tp_validate.iloc[0, 1], 1)
+        self.assertEqual(tp_validate.iloc[0, 2], 200)
+        self.assertEqual(tp_validate.iloc[0, 3], 20)
+        self.assertEqual(tp_validate.iloc[1, 0], 1)
+        self.assertEqual(tp_validate.iloc[1, 1], 4)
+        self.assertEqual(tp_validate.iloc[1, 2], 500)
+        self.assertEqual(tp_validate.iloc[1, 3], 50)
+        
+        # check test
+        self.assertEqual(tp_test.iloc[0, 0], 0)
+        self.assertEqual(tp_test.iloc[0, 1], 2)
+        self.assertEqual(tp_test.iloc[0, 2], 300)
+        self.assertEqual(tp_test.iloc[0, 3], 20)
+        self.assertEqual(tp_test.iloc[1, 0], 0)
+        self.assertEqual(tp_test.iloc[1, 1], 2)
+        self.assertEqual(tp_test.iloc[1, 2], 300)
+        self.assertEqual(tp_test.iloc[1, 3], 30)
+        self.assertEqual(tp_test.iloc[2, 0], 1)
+        self.assertEqual(tp_test.iloc[2, 1], 5)
+        self.assertEqual(tp_test.iloc[2, 2], 600)
+        self.assertEqual(tp_test.iloc[2, 3], 40)
+        self.assertEqual(tp_test.iloc[3, 0], 1)
+        self.assertEqual(tp_test.iloc[3, 1], 5)
+        self.assertEqual(tp_test.iloc[3, 2], 600)
+        self.assertEqual(tp_test.iloc[3, 3], 60)
         
     def test_leave_one_item(self):
         testdata = generate_data()
