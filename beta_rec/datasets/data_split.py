16fb32cf7976579be798b71e38bd6c2ad59ac006
leungyukshing <jacky14.liang@gmail.com>
2020-04-24 00:20:27 +0800

    fix: allow timestamp not exist when leave_one_out

--- beta_rec/datasets/data_split.py
+++ beta_rec/datasets/data_split.py
@@ -379,7 +379,8 @@ def leave_one_out(data, random=False):
     if random:
         data = sklearn.utils.shuffle(data)
     else:
-        data.sort_values(by=[DEFAULT_TIMESTAMP_COL], inplace=True)
+        if DEFAULT_TIMESTAMP_COL in data.columns:
+            data.sort_values(by=[DEFAULT_TIMESTAMP_COL], inplace=True)
 
     users = data[DEFAULT_USER_COL].unique()
     for u in tqdm(users):
