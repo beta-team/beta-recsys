394d748bef2fd20a1380befc9698c39baedd56bb
Alva112358 <liangjh45@mail2.sysu.edu.cn>
2020-04-25 19:45:36 +0800

    fix: change checked file path

--- beta_rec/datasets/instacart.py
+++ beta_rec/datasets/instacart.py
@@ -6,6 +6,9 @@ from beta_rec.utils.common_util import un_zip, timeit
 from beta_rec.utils.constants import *
 from beta_rec.datasets.dataset_base import DatasetBase
 
+# Download URL.
+INSTACART_URL = 'https://s3.amazonaws.com/instacart-datasets/instacart_online_grocery_shopping_2017_05_01.tar.gz'
+
 
 class Instacart(DatasetBase):
     def __init__(self):
@@ -15,48 +18,49 @@ class Instacart(DatasetBase):
         If the dataset can not be download by the url,
         you need to down the dataset by the link:
             'https://s3.amazonaws.com/instacart-datasets/instacart_online_grocery_shopping_2017_05_01.tar.gz'
-        then put it into the directory `instacart/raw`
+        then put it into the directory `instacart/raw`, unzip this file and rename the directory in 'instacart'.
 
         Instacart dataset is used to predict when users buy
         product for the next time, we construct it with structure [order_id, product_id] => 
         """
         super().__init__(
             'instacart',
-            url = 'https://s3.amazonaws.com/instacart-datasets/instacart_online_grocery_shopping_2017_05_01.tar.gz'
+            url=INSTACART_URL
         )
 
-    
     def preprocess(self):
         """Preprocess the raw file
 
         Preprocess the file downloaded via the url,
         convert it to a dataframe consist of the user-item interaction
         and save in the processed directory
+
+        Download and load datasets
+        1. Download instacart dataset if this dataset is not existed.
+        2. Load <order> table and <order_products> table from "orders.csv" and "order_products__train.csv".
+        3. Merge the two tables above.
+        4. Add additional columns [rating, timestamp].
+        5. Rename columns and save data model.
         """
-        
-        # If raw file doesn't exist, download it into your database.
-        file_zip_name = os.path.join(self.raw_path, "instacart.zip")
-        if not os.path.exists(file_zip_name):
+
+        # Step 1: Download instacart dataset if this dataset is not existed.
+        order_file_path = os.path.join(self.raw_path, self.dataset_name, "orders.csv")
+        order_product_file_path = os.path.join(self.raw_path, self.dataset_name, "order_products__train.csv")
+        if not os.path.exists(order_file_path) or not os.path.exists(order_product_file_path):
             print("Raw file doesn't exist, try to download it.")
             self.download()
 
-        # Process raw file
-        path_name = os.path.join(self.raw_path, self.dataset_name)  
-        # orders_table, containing order_id, user_id, and time.
-        # orders_products_table, containing product_id, order_id, and reordered info.
+        # Step 2: Load <order> table and <order_products> table from "orders.csv" and "order_products__train.csv".
+        path_name = os.path.join(self.raw_path, self.dataset_name)
         orders_data = os.path.join(path_name, "orders.csv")
         orders_products_data = os.path.join(path_name, "order_products__train.csv")
-        products_data = os.path.join(path_name, "products.csv")
 
         # orders_table
         orders_table = pd.read_csv(
             orders_data,
             usecols=[
                 "order_id", 
-                "user_id", 
-                #"eval_set",
-                #"order_number",
-                #"order_dow",
+                "user_id",
                 "order_hour_of_day", 
                 "days_since_prior_order"
             ]
@@ -67,59 +71,34 @@ class Instacart(DatasetBase):
             orders_products_data,
             usecols=[
                 "order_id", 
-                "product_id", 
-                #"add_to_cart_order", 
-                #"reordered"
+                "product_id",
             ],
         )
 
-        # products_table
-        # Table with info structure [product_id, aisle_id, department_id].
-        # products_table = pd.read_csv(
-        #     products_data,
-        #     usecols=[
-        #         "product_id", 
-        #         "aisle_id", 
-        #         "department_id"
-        #     ],
-        # )
-
-        # Merge all the four datasets according to order_id, products_id.
+        # Step 3: Merge the two tables above.
         prior_transactions = orders_products_table.merge(
             orders_table,
             left_on = "order_id",
             right_on = "order_id",
         )
 
-        # Merge the products_table
-        # prior_transactions = prior_transactions.merge(
-        #     products_table,
-        #     left_on = "product_id",
-        #     right_on = "product_id",
-        # )
-
+        # Step 5: Add additional columns [rating, timestamp].
         # Add rating columns into table.
         prior_transactions.insert(3, "rating", 1)
 
-        # Process time datas
+        # Create virtual timestamp and add to the merge table.
         virtual_date = datetime.datetime.strptime("2020-04-21 00:00", "%Y-%m-%d %H:%M")
         prior_transactions["days_since_prior_order"] = prior_transactions["days_since_prior_order"].apply(lambda t: (virtual_date + datetime.timedelta(days=t)).timestamp())
         prior_transactions = prior_transactions.drop(["order_hour_of_day"], axis=1)
 
-        # Standardize the columns' name.
+        # Step 6: Rename columns and save data model.
         prior_transactions.rename(
             columns = {
-                "user_id"                : DEFAULT_USER_COL,
-                "order_id"               : DEFAULT_ORDER_COL,
-                "product_id"             : DEFAULT_ITEM_COL,
-                "rating"                 : DEFAULT_RATING_COL, 
-                #"aisle_id"              : "aisle_id",
-                #"department_id"         : "department_id",
-                #"add_to_cart_order"     : "add_to_cart_order",
-                #"reordered"             : "reordered",
-                #"order_hour_of_day"     : "order_hour_of_day",
-                "days_since_prior_order" : DEFAULT_TIMESTAMP_COL,
-                #"time"                  : DEFAULT_TIMESTAMP_COL,
+                "user_id": DEFAULT_USER_COL,
+                "order_id": DEFAULT_ORDER_COL,
+                "product_id": DEFAULT_ITEM_COL,
+                "rating": DEFAULT_RATING_COL,
+                "days_since_prior_order": DEFAULT_TIMESTAMP_COL,
             },
             inplace = True,
         )
