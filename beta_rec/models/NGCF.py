ec6ddf915855d82e65bdf883341480a885a02086
TedSIWEILIU <750242739@qq.com>
2020-04-28 10:05:44 +0100

    First commit

---
+++ beta_rec/models/NGCF.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+import torch.sparse as sparse
+import torch.nn.functional as F
+import numpy as np
+from beta_rec.models.torch_engine import Engine
+from beta_rec.datasets.NGCF_data_utils import Data
+
+
+class NGCF(torch.nn.Module):
+    """Model initialisation, embedding generation and prediction of NGCF
+
+    """
+    def __init__(self, config):
+        super(NGCF, self).__init__()
+        self.config = config
+        self.num_users = config["num_users"]
+        self.num_items = config["num_items"]
+        self.emb_dim = config["emb_dim"]
+        self.layer_size = config['layer_size']
+        self.n_layers = len(self.layer_size)
+        self.dropout = nn.ModuleList()
+        self.GC_weights = nn.ModuleList()
+        self.Bi_weights = nn.ModuleList()
+        self.dropout_list = list(config['mess_dropout'])
+        self.layer_size = [self.emb_dim] + self.layer_size
+        # Create GNN layers
+        for i in range(self.n_layers):
+            self.GC_weights.append(nn.Linear(self.layer_size[i], self.layer_size[i + 1]))
+            self.Bi_weights.append(nn.Linear(self.layer_size[i], self.layer_size[i + 1]))
+            self.dropout.append(nn.Dropout(self.dropout_list[i]))
+
+        self.user_embedding = nn.Embedding(self.num_users, self.emb_dim)
+        self.item_embedding = nn.Embedding(self.num_items, self.emb_dim)
+        self.init_emb()
+
+    def init_emb(self):
+        # Initialise users and items' embeddings
+        nn.init.xavier_uniform_(self.user_embedding.weight)
+        nn.init.xavier_uniform_(self.item_embedding.weight)
+
+    def forward(self, norm_adj):
+        """ Perform GNN function on users and item embeddings
+        Args:
+            norm_adj (torch sparse tensor): the norm adjacent matrix of the user-item interaction matrix
+        Returns:
+            u_g_embeddings (tensor): processed user embeddings
+            i_g_embeddings (tensor): processed item embeddings
+        """
+        ego_embeddings = torch.cat((self.user_embedding.weight, self.item_embedding.weight), dim=0)
+        all_embeddings = [ego_embeddings]
+
+        for i in range(self.n_layers):
+            side_embeddings = torch.sparse.mm(norm_adj, ego_embeddings)
+            sum_embeddings = F.leaky_relu(self.GC_weights[i](side_embeddings))
+            bi_embeddings = torch.mul(ego_embeddings, side_embeddings)
+            bi_embeddings = F.leaky_relu(self.Bi_weights[i](bi_embeddings))
+            ego_embeddings = sum_embeddings + bi_embeddings
+            ego_embeddings = self.dropout[i](ego_embeddings)
+
+            norm_embeddings = F.normalize(ego_embeddings, p=2, dim=1)
+            all_embeddings += [norm_embeddings]
+
+        all_embeddings = torch.cat(all_embeddings, dim=1)
+        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.num_users, self.num_items], dim=0)
+        return u_g_embeddings, i_g_embeddings
+
+    def predict(self, users, items):
+        """ Model prediction: dot product of users and items embeddings
+        Args:
+            users (int):  user id
+            items (int):  item id
+        Return:
+            scores (int): dot product
+        """
+        users_t = torch.tensor(users, dtype=torch.int64, device=self.device)
+        items_t = torch.tensor(items, dtype=torch.int64, device=self.device)
+
+        with torch.no_grad():
+            scores = torch.mul(self.user_embedding(users_t),self.item_embedding(items_t)).sum(dim=1)
+        return scores
+
+
+class NGCFEngine(Engine):
+# A class includes train an epoch and train a batch of NGCF
+    def __init__(self, config):
+        self.config = config
+        self.model = NGCF(config)
+        self.regs = config["regs"]
+        self.decay = self.regs[0]
+        self.batch_size = config["batch_size"]
+        self.norm_adj = config["norm_adj"]
+        self.num_batch = config["num_batch"]
+
+        super(NGCFEngine, self).__init__(config)
+
+    def train_single_batch(self, batch_data):
+        """
+        Args:
+            batch_data (list): batch users, positive items and negative items
+        Return:
+            loss (float): batch loss
+        """
+        assert hasattr(self, "model"), "Please specify the exact model !"
+        self.optimizer.zero_grad()
+        norm_adj = self.norm_adj
+        ua_embeddings, ia_embeddings = self.model.forward(norm_adj)
+
+        batch_users, pos_items, neg_items = batch_data
+
+        u_g_embeddings = ua_embeddings[batch_users]
+        pos_i_g_embeddings = ia_embeddings[pos_items]
+        neg_i_g_embeddings = ia_embeddings[neg_items]
+
+        batch_mf_loss, batch_emb_loss, batch_reg_loss = self.bpr_loss(u_g_embeddings, pos_i_g_embeddings,
+                                                              neg_i_g_embeddings)
+
+        batch_loss = batch_mf_loss + batch_emb_loss + batch_reg_loss
+
+        batch_loss.backward()
+        self.optimizer.step()
+        loss = batch_loss.item()
+        return loss
+
+    def train_an_epoch(self, epoch_id,user,pos_i,neg_i):
+        """ Generate batch data for each batch
+        Args:
+            epoch_id (int):
+            user (list)
+            pos_i (list):
+            neg_i (list):
+        """
+        assert hasattr(self, "model"), "Please specify the exact model !"
+        self.model.train()
+        total_loss = 0.0
+        batch_data = (user, pos_i, neg_i)
+
+        n_batch = self.num_batch
+
+        for idx in range(n_batch):
+            loss = self.train_single_batch(batch_data)
+            total_loss += loss
+        print("[Training Epoch {}], Loss {}".format(epoch_id, loss))
+        self.writer.add_scalar("model/loss", total_loss, epoch_id)
+
+    def bpr_loss(self, users, pos_items, neg_items):
+        # Calculate BPR loss
+        pos_scores = torch.sum(torch.mul(users, pos_items), dim=1)
+        neg_scores = torch.sum(torch.mul(users, neg_items), dim=1)
+
+        regularizer = 1./2*(users**2).sum() + 1./2*(pos_items**2).sum() + 1./2*(neg_items**2).sum()
+
+        regularizer = regularizer / self.batch_size
+
+        maxi = F.logsigmoid(pos_scores - neg_scores)
+        mf_loss = -torch.mean(maxi)
+
+        emb_loss = self.decay * regularizer
+        reg_loss = 0.0
+
+        return mf_loss, emb_loss, reg_loss
---
+++ examples/train_NGCF.py
@@ -0,0 +1,107 @@
+import sys
+from beta_rec.datasets.dunnhumby import Dunnhumby
+sys.path.append("../")
+import os
+import json
+import numpy as np
+import argparse
+import torch
+import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
+from beta_rec.models.NGCF import NGCF
+from beta_rec.models.NGCF import NGCFEngine
+from beta_rec.datasets.NGCF_data_utils import Data
+from beta_rec.datasets import dataset
+from beta_rec.datasets.movielens import Movielens_100k
+
+
+def parse_args():
+    """
+    Parse args from command line
+    Returns:
+
+    """
+    parser = argparse.ArgumentParser(description="Run NGCF..")
+    parser.add_argument(
+        "--config_file",
+        nargs="?",
+        type=str,
+        default="../configs/NGCF_default.json",
+        help="Specify the config file name. Only accept a file from ../configs/",
+    )
+
+    parser.add_argument(
+        "--emb_dim", nargs="?", type=int, help="Dimension of the embedding."
+    )
+    parser.add_argument("--lr", nargs="?", type=float, help="Initialize learning rate.")
+    parser.add_argument("--num_epoch", nargs="?", type=int, help="Number of max epoch.")
+
+    parser.add_argument(
+        "--batch_size", nargs="?", type=int, help="Batch size for training."
+    )
+    return parser.parse_args()
+
+def update_args(config, args):
+    """Update config parameters by the received parameters from command line
+
+        Args:
+            config (dict): Initial dict of the parameters from JOSN config file.
+            args (object): An argparse Argument object with attributes being the parameters to be updated.
+
+        Returns:
+            None
+    """
+    for k, v in vars(args).items():
+        if v != None:
+            config[k] = v
+            print("Received parameters form comand line:", k, v)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(args)
+    config_file = args.config_file
+    with open(config_file) as config_params:
+        config = json.load(config_params)
+
+    dataset = Movielens_100k()
+    dataset.preprocess()
+    train, vad, test = dataset.load_leave_one_out(n_test=1)
+    # ToDo: Please define the directory to store the adjacent matrix
+    data_loader = Data(path=dataset.dataset_dir,train=train, test=test[0], vad=vad[0],batch_size=int(config["batch_size"]))
+    plain_adj, norm_adj, mean_adj = data_loader.get_adj_mat()
+    norm_adj = sparse_mx_to_torch_sparse_tensor(norm_adj)
+    vad = data_loader.vad
+    test = data_loader.test
+
+    config["norm_adj"] = norm_adj
+    config["num_batch"] = data_loader.n_train // config["batch_size"] + 1
+    config["num_users"] = data_loader.n_users
+    config["num_items"] = data_loader.n_items
+    engine = NGCFEngine(config)
+
+    counter = 0
+    for epoch in range(config["num_epoch"]):
+        users, pos_items, neg_items = data_loader.sample()
+        engine.train_an_epoch(epoch_id=epoch, user=users,pos_i=pos_items,neg_i=neg_items)
+    # ToDo: Please enable early stop, log, time monitor
+        counter+=1
+        if counter % 1 == 0:
+            result = engine.evaluate(eval_data_df=vad,epoch_id=epoch)
+            print(result)
+    # ToDo: Please save model and result file
+
+
+
+
+
