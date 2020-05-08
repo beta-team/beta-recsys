454213bdbb50d642f995471d4d69a9a313bb6652
leungyukshing <jacky14.liang@gmail.com>
2020-05-09 01:38:57 +0800

    fix: fix test err

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,4 +6,4 @@ class TestDownload(unittest.TestCase):
     def test_get_format(self):
         self.assertEqual(get_format("gz"), "gztar")
         self.assertEqual(get_format("gz2"), "gztar")
-        self.assertEqual(get_format("bz2"), "gztar")
+        self.assertEqual(get_format("bz2"), "bztar")
