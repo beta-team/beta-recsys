7f1201945b3ac68f6d2f475de50e85f705abde89
leungyukshing <jacky14.liang@gmail.com>
2020-05-10 23:51:26 +0800

    chore: add implicit datasets

--- beta_rec/utils/constants.py
+++ beta_rec/utils/constants.py
@@ -10,3 +10,7 @@ DEFAULT_PREDICTION_COL = "col_prediction"
 DEFAULT_K = 10
 DEFAULT_THRESHOLD = 10
 MAX_N_UPDATE = 5  # ealy stop criterion, max number of epoches having no update
+
+# implicit datasets (score being 1)
+IMPLIICIT_DATASETS = ["ali_mobile", "citeulike-a", "citeulike-t", "diginetica", "dunnhumby", "gowalla", "delicious-2k",
+                      "retailrocket", "tafeng", "taobao", "yoochoose"]
