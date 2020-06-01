a5e66a4605d20d75a58a9c430c1855ba50ffe421
leungyukshing <jacky14.liang@gmail.com>
2020-06-02 02:20:34 +0800

    fix: documentation support md table

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "recommonmark",
+    "sphinx_markdown_tables",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
