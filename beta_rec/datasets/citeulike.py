ab6509bb138fa7abf1ce076f8b20279e6a75a8ee
leungyukshing <jacky14.liang@gmail.com>
2020-06-20 00:00:52 +0800

    chore: black file

--- beta_rec/datasets/citeulike.py
+++ beta_rec/datasets/citeulike.py
@@ -189,7 +189,9 @@ class CiteULikeT(DatasetBase):
 
         print("Done.")
 
-    def load_leave_one_out(self, random=False, n_negative=100, n_test=10, download=False):
+    def load_leave_one_out(
+        self, random=False, n_negative=100, n_test=10, download=False
+    ):
         r"""Load leave one out split data."""
         if random is False:
             raise RuntimeError(
