ad4f37e7f32695ce33a9f2790590731f5b0c2947
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2020-11-15 23:55:57 +0000

    feat: implement triple2vec with new Recommender api

--- beta_rec/recommenders/__init__.py
+++ beta_rec/recommenders/__init__.py
@@ -1,4 +1,8 @@
 """Beta_rec Recommender."""
+from .lightgcn import LightGCN
 from .matrix_factorization import MatrixFactorization
+from .ncf import NeuCF
+from .ngcf import NGCF
+from .triple2vec import Triple2vec
 
-__all__ = ["MatrixFactorization"]
+__all__ = ["MatrixFactorization", "NeuCF", "Triple2vec", "LightGCN", "NGCF"]
