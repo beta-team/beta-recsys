bea9c50f73e17f9a8d15069efc6f33db8ea1816e
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2021-01-23 00:54:04 +0000

    chore: format codes

--- beta_rec/recommenders/sasrec.py
+++ beta_rec/recommenders/sasrec.py
@@ -99,8 +99,7 @@ def tune_train(config):
     while train_engine.eval_engine.n_worker > 0:
         time.sleep(20)
     tune.track.log(
-        valid_metric=result["valid_metric"],
-        model_save_dir=result["model_save_dir"],
+        valid_metric=result["valid_metric"], model_save_dir=result["model_save_dir"],
     )
 
 
