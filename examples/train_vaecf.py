6fd593ede0aee359baa289b97c239b09ff2765f9
siwei <36344915+TedSIWEILIU@users.noreply.github.com>
2021-03-06 19:44:57 +0800

    Update train_vaecf.py

--- examples/train_vaecf.py
+++ examples/train_vaecf.py
@@ -68,12 +68,14 @@ def parse_args():
         "--lr",
         nargs="?",
         type=float,
-        help="Initial learning rate")
+        help="Initial learning rate"
+    )
     parser.add_argument(
         "--max_epoch",
         nargs="?",
         type=int,
-        help="Number of max epoch")
+        help="Number of max epoch"
+    )
     parser.add_argument(
         "--batch_size",
         nargs="?",
